364a735de8e351b90b36cf896b7b2fb19e532e3b
yufeiwang63 <yufeiwang63@gmail.com>
2026-05-18 23:30:20 -0400

    update

--- prepare.sh
+++ prepare.sh
@@ -1,6 +1,20 @@
-export HF_LEROBOT_HOME="${PWD}/data"
-export HF_HOME="${PWD}/data/cache/huggingface"
+path=/tmp
+# path="${PWD}"
+export HF_LEROBOT_HOME="${path}/data"
+export HF_HOME="${path}/data/cache/huggingface"
 export HF_DATASETS_CACHE=$HF_HOME/datasets
-export OPENPI_DATA_HOME="${PWD}/data/cache/openpi/"
-export OPENPI_JAX_CACHE_DIR="${PWD}/data/cache/jax"
-export UV_CACHE_DIR="${PWD}/data/cache/"
+export OPENPI_DATA_HOME="${path}/data/cache/openpi/"
+export OPENPI_JAX_CACHE_DIR="${path}/data/cache/jax"
+export UV_CACHE_DIR="${path}/data/cache/"
+echo "HF_LEROBOT_HOME: $HF_LEROBOT_HOME"
+echo "HF_HOME: $HF_HOME"
+echo "HF_DATASETS_CACHE: $HF_DATASETS_CACHE"
+echo "OPENPI_DATA_HOME: $OPENPI_DATA_HOME"
+echo "OPENPI_JAX_CACHE_DIR: $OPENPI_JAX_CACHE_DIR"
+echo "UV_CACHE_DIR: $UV_CACHE_DIR"
+mkdir -p $HF_LEROBOT_HOME
+mkdir -p $HF_HOME
+mkdir -p $HF_DATASETS_CACHE
+mkdir -p $OPENPI_DATA_HOME
+mkdir -p $OPENPI_JAX_CACHE_DIR
+mkdir -p $UV_CACHE_DIR
